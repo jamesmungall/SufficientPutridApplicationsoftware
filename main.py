7f825e213f7489f15a75884ee4a9bb9fce795d0c
jamesmungall <preanuts26@gmail.com>
2024-10-01 12:51:49 +0000

    stars less than 23 parsecs added

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from astroquery.gaia import Gaia
 import pandas as pd
 
-query = ("SELECT "
+q1 = ("SELECT "
         "source_id, ra, dec, distance_gspphot, "
         "phot_g_mean_mag, teff_gspphot "
         "FROM gaiadr3.gaia_source_lite "
         "WHERE distance_gspphot < 23 "
-        "AND phot_g_mean_mag < 4 "
+        "AND phot_g_mean_mag < 5 "
         "ORDER BY distance_gspphot")
 
 q2 = ("SELECT  distance_gspphot "
@@ -16,8 +16,8 @@ q2 = ("SELECT  distance_gspphot "
       "AND distance_gspphot < 3000 "
      )
       
-job = Gaia.launch_job(q2)
+job = Gaia.launch_job(q1)
 results = job.get_results()
 df = results.to_pandas()
-df.to_csv('gaia_source_lite.csv', index=False)
+df.to_csv('stars_0_23pc.csv', index=False)
 
