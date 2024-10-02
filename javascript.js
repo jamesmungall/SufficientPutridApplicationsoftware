1e2d705ab2bd5b9acd73e4aa1bff07932ffed73d
jamesmungall <preanuts26@gmail.com>
2024-10-02 09:22:14 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
+// Selecting the iframe element
+var frame = document.getElementById("Iframe");
+            
+// Adjusting the iframe height onload event
+frame.onload = function() {
+                // set the height of the iframe as 
+                // the height of the iframe content
+                frame.style.height = frame.contentWindow.document.body.scrollHeight + 'px';
+
+                // set the width of the iframe as the 
+                // width of the iframe content
+                frame.style.width  = frame.contentWindow.document.body.scrollWidth + 'px';
+  
