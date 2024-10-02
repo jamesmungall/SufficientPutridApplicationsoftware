a3855709ca292101b406c9aff89ef241a3d83fd0
jamesmungall <preanuts26@gmail.com>
2024-10-02 09:38:16 +0100

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,16 +1,6 @@
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
-// Selecting the iframe element
-var frame = document.getElementById("Iframe");
-            
-// Adjusting the iframe height onload event
-frame.onload = function() {
-                // set the height of the iframe as 
-                // the height of the iframe content
-                frame.style.height = frame.contentWindow.document.body.scrollHeight + 'px';
-
-                // set the width of the iframe as the 
-                // width of the iframe content
-                frame.style.width  = frame.contentWindow.document.body.scrollWidth + 'px';
-  
+function get_csv(){
+  document.getElementById("get_cvs").innerHTML = "HTML request here";
+}
