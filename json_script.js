3022fffd9c2d208f373d4718be577ab426df8072
jamesmungall <preanuts26@gmail.com>
2024-10-03 16:18:01 +0100

    Update json_script.js

--- json_script.js
+++ json_script.js
@@ -1,21 +1,20 @@
-async function fetchJsonData(filename) {
+async function fetchCsvData() {
     try {
-        const response = await fetch('stars_0_23pc.json');
+        const response = await fetch('data.csv');
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
-        const data = await response.json();
-        return JSON.parse(data)
-        
+        const csvText = await response.text();
+        const data = Papa.parse(csvText, {
+            header: true,
+            skipEmptyLines: true
+        });
+
+        const csvArray = data.data; // This will be an array of objects
+        document.getElementById('csvData').textContent = JSON.stringify(csvArray, null, 2);
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
     }
-    
-    
 }
 
-json_obj = fetchJsonData('stars_0_23pc.json');
-
-//document.getElementById('jsonData').textContent = json_obj.SOURCE_ID;
-document.getElementById('ra').textContent = json_obj.ra;
-//document.getElementById('dec').textContent = json_obj.dec;
+fetchCsvData();
