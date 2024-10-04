d1f2fbc4fea90c2a62541ba348cddb477bd27b32
jamesmungall <preanuts26@gmail.com>
2024-10-04 10:16:43 +0100

    Create navigation.js

---
+++ navigation.js
@@ -0,0 +1,20 @@
+function injectCommonHtml() {
+    const commonHtml = `
+        <header>
+        <nav>
+    <a href="index.html">Home</a>
+    <a href="plotly_chart.html">Plotly Chart</a>
+    <a href="ellipse.html">Ellipse</a>
+    <a href="stars_0_23pc.csv">CSV for stars within 23pc</a>
+    <a href="read_csv.html">Read csv</a>
+    <a href="get_file.html">Get file</a>
+	</nav>
+        </header>
+    `;
+
+    // Injecting the HTML into the body of the document
+    document.body.insertAdjacentHTML('afterbegin', commonHtml);
+}
+
+// Call the function to inject the HTML when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', injectCommonHtml);
