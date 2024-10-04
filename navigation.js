58048dbf369d622ba4271bbf0dfa2da9841f7844
jamesmungall <preanuts26@gmail.com>
2024-10-04 10:38:04 +0100

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -3,7 +3,7 @@ function injectCommonHtml() {
         <header>
         <nav>
     <a href="index.html">Home</a>
-    <a href="plotly_chart.html">Plotly Chart</a>
+    <a href="plotly_chart2.html">Plotly Chart 2</a>
     <a href="ellipse.html">Ellipse</a>
     <a href="stars_0_23pc.csv">CSV for stars within 23pc</a>
     <a href="read_csv.html">Read csv</a>
