8476a18894d8ce75aba5f97c56bcc8953090c6ea
jamesmungall <preanuts26@gmail.com>
2024-10-04 12:02:20 +0100

    Update json_script.js

--- json_script.js
+++ json_script.js
@@ -1,4 +1,4 @@
-async function fetchJsonData() {
+async function get_json() {
   try {
     const response = await fetch(
       "https://jamesmungall.github.io/SufficientPutridApplicationsoftware/stars_0_23pc.json"
@@ -7,16 +7,38 @@ async function fetchJsonData() {
       throw new Error("Network response was not ok " + response.statusText);
     }
     const data = await response.json();
-    document.getElementById("ra").textContent = Object.values(data.ra);
-    document.getElementById("dec").textContent = Object.values(data.dec);
-    document.getElementById("jsonData").textContent = JSON.stringify(
-      data,
-      null,
-      2
-    );
+    use_json(data);
   } catch (error) {
     console.error("There has been a problem with your fetch operation:", error);
   }
 }
 
-fetchJsonData();
+function use_json(data) {
+  ra_array = Object.values(data.ra);
+  document.getElementById("ra").textContent = ra_array;
+
+  dec_array = Object.values(data.dec);
+  document.getElementById("dec").textContent = dec_array;
+
+  run_plotly(ra_array, dec_array);
+}
+
+function run_plotly(ra_array, dec_array) {
+  TESTER = document.getElementById("plotly_div");
+
+  Plotly.newPlot(
+    TESTER,
+    [
+      {
+        x: ra_array,
+
+        y: dec_array
+      }
+    ],
+    {
+      margin: { t: 0 }
+    }
+  );
+}
+
+get_json();
