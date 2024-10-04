60c3d5f1c689b7c31d72d7c99cffca886f5c4ab5
jamesmungall <preanuts26@gmail.com>
2024-10-04 11:29:58 +0100

    Update json_script.js

--- json_script.js
+++ json_script.js
@@ -1,14 +1,22 @@
 async function fetchJsonData() {
-    try {
-        const response = await fetch('https://jamesmungall.github.io/SufficientPutridApplicationsoftware/data.json');
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        const data = await response.json();
-        document.getElementById('jsonData').textContent = JSON.stringify(data, null, 2);
-    } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
+  try {
+    const response = await fetch(
+      "https://jamesmungall.github.io/SufficientPutridApplicationsoftware/stars_0_23pc.json"
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
     }
+    const data = await response.json();
+    document.getElementById("ra").textContent = Object.values(data.ra);
+    document.getElementById("dec").textContent = Object.values(data.dec);
+    document.getElementById("jsonData").textContent = JSON.stringify(
+      data,
+      null,
+      2
+    );
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
 }
 
 fetchJsonData();
